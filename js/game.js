708d05aa1a427e434e524e73d172ad6597c54059
jop de winter <99039923@mydavinci.nl>
2016-11-02 20:44:26 +0100

    added sounds, gemaakt t/m level 3

--- js/game.js
+++ js/game.js
@@ -1,14 +1,34 @@
+//Variables
+let key = false;
+let removedMusic = false;
+
 // Shortcuts
 function get(id)
 {
 	return document.getElementById(id)
 }
 
-//Levels
-function start()
+//Functions
+function takeKey() 
 {
-	console.log("start()");
-	get("tooltiptext").innerHTML = "Uitleg:<br><br>Je zit s'avonds thuis tv te kijken. Er wordt bij je ingebroken. Door het spel heen krijg je verschillende keuzes en je moet de goede kiezen.";
+	key = true;
+	get("zaklamp").style.display = "none";
+}
+
+function keyCheck()
+{
+	if (key == true) {
+		level4();
+	} else {
+		get("warning").innerHTML = "Je hebt nog geen sleutel blyat";
+	}
+}
+
+//Levels
+function onLoad()
+{
+	document.getElementById("option1").className = 'option1';
+	get("option3").style.display = "none";
 }
 
 function level1()
@@ -18,11 +38,49 @@ function level1()
 	get("title").style.display = "none";
 	get("tooltip").style.display = "none";
 	get("option1").style.display = "none";
-	get("level1tekst").style.display = "inherit"
-	get("option2").style.display = "inherit"
+	get("option2").style.display = "inherit";
+	get("option2").setAttribute("onclick", "level2()");
+	get("option2").innerHTML = "Verder";
+	document.getElementById("music1").remove();
+	get("srcmusic").setAttribute("src","./sounds/music.mp3");
+	get("text").innerHTML = "Je zit een avondje tv te kijken tot plots het stroom uitvalt.";
 }
 
 function level2()
+{	
+	if (removedMusic === false) {
+		document.getElementById("music").remove();
+	}
+	document.body.style.background = "url(./img/keuken.jpg)";
+	document.body.style.backgroundSize = "100%";
+	get("option2").style.display = "none";
+	get("text").innerHTML = "Je wilt de kelder in om te kijken of je het stroom weer aan kan zetten, maar het is daar donker dus je hebt een zaklamp nodig.";
+	get("option3").innerHTML = "Open lade";
+	get("option3").style.display = "inherit";
+	get("option2").style.display = "inherit";
+	get("option2").innerHTML = "Ga naar kelder";
+	get("sleutels").style.display = "none";
+	get('option2').setAttribute("onclick", "keyCheck()");
+	get('option3').setAttribute("onclick", "level3()");
+	get("zaklamp").style.display = "none";
+}	
+
+function level3()
+{	
+	removedMusic = true;
+	get("warning").innerHTML = "";
+	document.body.style.background = "url(./img/lade.jpg)";
+	document.body.style.backgroundSize = "100%";
+	get("zaklamp").style.display = "inherit";
+	get("option2").style.display = "none"
+	get("sleutels").style.display = "inherit";
+	get("zaklamp").setAttribute("onclick", "takeKey()");
+	get("text").innerHTML = "Pak de zaklamp";
+	get("option3").innerHTML = "Terug";
+	get("option3").setAttribute("onclick", "level2()")
+}
+
+function level4() 
 {
-	
+	console.log("hey");
 }
